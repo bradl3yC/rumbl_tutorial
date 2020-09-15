8f147af4be8039f643cfd40f6e58fcbbb4962fd1
Brad Conley <brad.conl3y@gmail.com>
2020-09-15 15:09:52 -0400

    fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rumbl, RumblWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   load_from_system_env: true,
   url: [scheme: "https", host: "sheltered-sierra-96649.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
