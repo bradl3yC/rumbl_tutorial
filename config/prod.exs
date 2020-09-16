91c5ceccbe9c8999ce7527ce969d0c76d5d2350d
Brad Conley <brad.conl3y@gmail.com>
2020-09-16 09:25:18 -0400

    change config

--- config/prod.exs
+++ config/prod.exs
@@ -22,9 +22,9 @@ config :logger, level: :info
 
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true,
-  url: System.get_env("DATABASE_URL")
+  ssl: true
 
 # ## SSL Support
 #
