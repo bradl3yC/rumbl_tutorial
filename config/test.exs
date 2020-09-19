be2cc44cf9c5141955e135e1ac9cb02365a3754f
Brad Conley <brad.conl3y@gmail.com>
2020-09-19 12:32:54 -0400

    remove generated tests and add account tests

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :rumbl, Rumbl.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "bradleyryan",
+  password: "",
   database: "rumbl_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
